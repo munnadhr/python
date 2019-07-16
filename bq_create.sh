e2bc528e068f4fb0a53d5e21c8b98b8d5eb22f59
T3ch42o <habeebmunna12@gmail.com>
2019-07-16 12:21:25 +0530

    Update bq_create.sh

--- bq_create.sh
+++ bq_create.sh
@@ -2,18 +2,29 @@
 
 export Project=$1
 export Region=$2
+current_project=$(gcloud config get-value project)
 
-bq mk $3
-if [ $? -eq 0 ];then
-    echo "Dataset $3 created under $Project"
-    bq mk $3.$4
-    if [ $? -eq 0 ];then
-        echo "Table $4 has been created under Dataset $3"
-    else
-        echo "Table $4 is not created"
-        exit
-    fi
+function bq_create() {
+        bq mk $3
+        if [ $? -eq 0 ];then
+            echo "Dataset $3 created under $Project"
+            bq mk $3.$4
+            if [ $? -eq 0 ];then
+                echo "Table $4 has been created under Dataset $3"
+            else
+                echo "Table $4 is not created"
+                exit
+            fi
+        else
+            echo "Dataset $3 unable to create"
+            exit
+        fi
+ }
+
+if [ "$current_project" == "$Project" ];then
+    bq_create()
 else
-    echo "Dataset $3 unable to create"
-    exit
+    gcloud config set project $Project
+    bq_create()
 fi
+    
