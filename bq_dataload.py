9942c382529511fa47164667d79a9e680aa24f97
T3ch42o <habeebmunna12@gmail.com>
2019-07-16 15:45:05 +0530

    Update bq_dataload.py

--- bq_dataload.py
+++ bq_dataload.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
+import sys
 client = bigquery.Client()
-dataset_id = 'my_dataset'
+dataset_id = 'sys.argv[1]'
 
 dataset_ref = client.dataset(dataset_id)
 job_config = bigquery.LoadJobConfig()
@@ -10,12 +11,12 @@ job_config.skip_leading_rows = 1
 job_config.source_format = bigquery.SourceFormat.CSV
 uri = "gs://openshift-destination/munna_data.csv"
 load_job = client.load_table_from_uri(
-    uri, dataset_ref.table("munna_data"), job_config=job_config
+    uri, dataset_ref.table(sys.argv[2]), job_config=job_config
 )  # API request
 print("Starting job {}".format(load_job.job_id))
 
 load_job.result()  # Waits for table load to complete.
 print("Job finished.")
 
-destination_table = client.get_table(dataset_ref.table("munna_data"))
+destination_table = client.get_table(dataset_ref.table(sys.argv[2]))
 print("Loaded {} rows.".format(destination_table.num_rows))
